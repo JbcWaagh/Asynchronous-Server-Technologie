c0e2aca13563940944540fed6e30f2db57d0e053
Romain Michau <romain.michau@edu.ece.fr>
2018-11-14 17:40:18 +0100

    suprression handle.js

--- index.js
+++ index.js
@@ -1,4 +1,41 @@
-const http = require('http')
-const handles = require('./handles')
-const server = http.createServer(handles.serverHandle);
-server.listen(8080)
+express = require('express')
+app = express()
+
+
+const mesInfo="Bonjour, je suis Romain un etudiant de l'ECE en cybersecurite";
+const descSite="Bienvenue sur Hello. \n Pour plus d'information sur moi rejoignez l'url '/hello' avec mon nom (romain) comme parametre 'name'.  Exemple: localhost:8080/hello?name=romain\n "+
+  				"Sinon vous pouvez mettre le votre pour que je vous salue";
+const helloInc="Hello my friend";
+const e404='Erreur 404: Ressource not found :(';
+
+app.set('port', 8080)
+
+app.listen(
+  app.get('port'), () => console.log(`server listening on ${app.get('port')}`)
+)
+
+app.get(
+  '/hello',
+  (req, res) => res.send(helloInc)
+)
+
+app.get(
+  '/hello/:name',
+  (req, res) => {
+    if (req.params.name == 'romain'){
+      res.send(mesInfo)
+    } else {
+      res.send("Hello " + req.params.name)
+    }
+  }
+)
+
+
+app.get(
+  '/',
+  (req, res) => res.send(descSite)
+)
+
+app.use(function(req, res){
+       res.send(e404);
+   });
