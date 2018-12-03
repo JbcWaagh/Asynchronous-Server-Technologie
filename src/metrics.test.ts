13cb7f0f534114d29e7521e74b039ff6c61b804d
Romain Michau <romain.michau@edu.ece.fr>
2018-12-03 11:19:46 +0100

    test unitaire okay

--- src/metrics.test.ts
+++ src/metrics.test.ts
@@ -1,66 +1,64 @@
-  import "chai/register-should"
-  import { expect } from 'chai'
-  import { Metric, MetricsHandler } from './metrics'
-  import { LevelDb } from './leveldb'
+import "chai/register-should"
+import {
+    expect
+} from 'chai'
+import {
+    Metric,
+    MetricsHandler
+} from './metrics'
+import {
+    LevelDb
+} from './leveldb'
 
-  const a: number = 0
+const a: number = 0
 
 
-  const dbPath: string = 'db_test'
-  var dbMet: MetricsHandler
+const dbPath: string = 'db_test'
+var dbMet: MetricsHandler
 
-  describe('Metrics', function () {
-    before(function () {
-      LevelDb.clear(dbPath)
-      dbMet = new MetricsHandler(dbPath)
+describe('Metrics', function() {
+    before(function() {
+        LevelDb.clear(dbPath)
+        dbMet = new MetricsHandler(dbPath)
     })
 
-    describe('#get', function () {
-    it('should get empty array on non existing group', function () {
-      dbMet.get("0", function (err: Error | null, result?: Metric[]) {
-        expect(err).to.be.null
-        expect(result).to.not.be.undefined
-      //  console.log("result:")
-        expect(result).to.be.empty
-      })
+    describe('#get', function() {
+        it('should get empty array on non existing group', function() {
+            dbMet.get("0", function(err: Error | null, result ? : Metric[]) {
+                expect(err).to.be.null
+                expect(result).to.not.be.undefined
+                //  console.log("result:")
+                expect(result).to.be.empty
+            })
+        })
     })
-  })
 
-  describe('#save', function () {
-  it('should save data', function () {
+    describe('#save', function() {
+        it('should save data', function(done) {
 
-    dbMet.save("1", [ { timestamp: '1384686660000', value: 11 } ],function (err: Error | null) {
-      expect(err).to.be.null
+            dbMet.save("1", [{
+                timestamp: '1384686660000',
+                value: 11
+            }], function(err: Error | null) {
+                expect(err).to.be.null
+                done()
 
+            })
+        })
     })
-  })
-  it('should get the metric we just created', function (done) {
-    dbMet.get("1", function (err: Error | null, result?: Metric[]) {
-      var met: Metric[] = [new Metric('1384686660000',11)];
-    //  var met: Metric=
-      var ts : string=result![0].timestamp
-      var val: number=result![0].value
-      expect(err).to.be.null
-      expect(result).to.not.be.undefined
-      expect(result).to.not.be.empty
 
-  //    console.log(met[0])
-        console.log(result)
-      setTimeout(function(){
-        done()
-        console.log(result)
-
-        expect(ts).to.equal('1384686660d000')
-        expect(val).to.equal(11)
-      },100)
-
-
+    describe('#Delete', function() {
+        it('should delete a data', function(done) {
+            dbMet.delete('11', (err: Error | null) => {
+              expect(err).to.be.null
+              done()
+            })
+        })
     })
-  })
-  })
 
-    after(function () {
-    //  console.log("fin")
-    //  LevelDb.clear(dbPath)
+
+    after(function() {
+        //  console.log("fin")
+        //  LevelDb.clear(dbPath)
     })
-  })
+})
