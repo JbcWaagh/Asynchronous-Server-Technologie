226382cac8b9a7467cabe0cc0eeb0589fc2c7b18
Romain Michau <romain.michau@edu.ece.fr>
2018-12-07 11:25:46 +0100

    nouveau gitignore

--- src/server.ts
+++ src/server.ts
@@ -44,12 +44,10 @@ app.use(morgan('dev'))
 app.use(bodyParser.urlencoded())
 app.use(express.static(path.join(__dirname, '/../public')))
 
-app.use('/metrics', metricsRouter)
 app.use('/dev', devRouter)
 app.use('/auth', authRouter)
 
 app.get('/', authCheck,(req: any, res: any) => {
-    //console.log(req.session.user)
     if(req.session.loggedIn){
         res.render('index', {name: req.session.user.username})}
     else {
@@ -79,16 +77,20 @@ authRouter.get('/login', (req: any, res: any) => {
 })
 
 authRouter.post('/login', (req: any, res: any, next: any) => {
-    dbUser.get(req.body.username, (err: Error | null, result?: User) => {
-        if (err) res.status(401).send("Mauvais username ou MDP")
-        else if (result === undefined || !result.validatePassword(req.body.password)) {
-            res.status(401).send("mdp ou login inccorrect")
-        } else {
-            req.session.loggedIn = true
-            req.session.user = result
-            res.redirect('/')
-        }
-    })
+    if(req.body.username===""||req.body.password==="")
+        res.status(401).send("merci de remplir tous les champs")
+    else{
+        dbUser.get(req.body.username, (err: Error | null, result?: User) => {
+            if (err) res.status(401).send("Mauvais username ou MDP")
+            else if (result === undefined || !result.validatePassword(req.body.password)) {
+                res.status(401).send("mdp ou login inccorrect")
+            } else {
+                req.session.loggedIn = true
+                req.session.user = result
+                // res.status(200).send
+                res.redirect('/')
+            }
+        })}
 })
 
 authRouter.post('/signup', (req: any, res: any, next: any) => {
@@ -120,54 +122,47 @@ authRouter.get('/signup',(req:any,res:any,next:any)=>{
 /////               user Router                       //////////////
 ////routes pour les users connectés                     /////////////
 ////////////////////////////////////////////////////////////////////
+app.use('/metrics', metricsRouter)
 
 
-metricsRouter.use(function (req: any, res: any, next: any) {
-    authCheck(req,res,next)
-})
 
 metricsRouter.get('/',(req:any,res:any,next:any)=>{
-//    res.render('index', {name: req.session.user.username})}
+
     metricsHandler.get(req.session.user.username,(err: Error|null,metrics:any)=>{
+        console.log("access metrics")
         if(err)
             throw err
         else{
-         //   console.log(metrics)
-
-            res.render('metrics',{metrics:metrics})}
+         //   console.log(user)
+            res.render('metrics',{metrics:metrics,user:req.session.user.username})}
     })
-  //  console.log('metrics')
-
 })
 
-
-
 metricsRouter.post('/del/',(req:any,res:any,next:any)=>{
-   // console.log("dellete router")
-   // console.log(req.body.timestamp)
     metricsHandler.delete(req.session.user.username,req.body.timestamp,(err: Error|null)=>{
         if(err)
             throw err
         else{
-          //  console.log("delete")
-            //res.render('metrics',{metrics:metrics})}
             res.redirect('/metrics/')
         }
-
     })
-
 })
-
 
 metricsRouter.post ('/',(req:any,res:any,next:any)=>{
-    metricsHandler.save(req.session.user.username,[req.body],(err:Error|null)=>{
-        if(err)
-            res.status(401).send("le metrics n'a pas pu être sauvegardé" )
-        else
-            res.redirect('/metrics/')
-    })
+    if(req.body.timestamp===""||req.body.value==="")
+        res.status(400).send("merci de remplir tous les champs")
+    else{
+        metricsHandler.save(req.session.user.username,[req.body],(err:Error|null)=>{
+            if(err)
+                res.status(401).send("le metrics n'a pas pu être sauvegardé" )
+            else
+                res.redirect('/metrics/')
+        })}
 })
 
+metricsRouter.use(function (req: any, res: any, next: any) {
+    authCheck(req,res,next)
+})
 
 ////////////////////////////////////////////////////////////////////
 /////               dev Router                       //////////////
@@ -194,32 +189,3 @@ devRouter.get('/del',(req:any,res:any,next:any)=>{
         res.status(200).json(err)
     })
 })
-
-/*
-app.delete('/metrics/:id', (req: any, res: any) => {
-    metricsHandler.delete(req.params.id,(err: Error | null) => {
-        if (err) {
-            throw err
-        }
-        res.status(200).send();
-    })
-})
-
-app.post('/metrics/:id', (req: any, res: any) => {
-
-    metricsHandler.save(req.params.id,req.body,(err: Error | null) => {
-        if (err) {
-            throw err
-        }
-        res.status(200).send();
-    })
-})
-
-app.get('/metrics/:id', (req: any, res: any) => {
-    metricsHandler.get(   req.params.id,(err: Error | null, result?: any) => {
-        if (err) {
-            throw err
-        }
-        res.json(result)
-    })
-})*/
